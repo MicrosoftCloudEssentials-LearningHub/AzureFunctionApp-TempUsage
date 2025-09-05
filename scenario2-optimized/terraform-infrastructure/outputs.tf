f76a0d3cc9f2d39a9f615c54a909d098b589e5f3
Timna Brown <24630902+brown9804@users.noreply.github.com>
2025-09-05 15:23:04 -0600

    Add outputs for Terraform deployment
    This file defines various outputs for the Terraform deployment, including resource group name, function app name, and storage account names.

---
+++ scenario2-optimized/terraform-infrastructure/outputs.tf
@@ -0,0 +1,68 @@
+# outputs.tf
+# This file contains the outputs from the Terraform deployment for the optimized scenario
+
+output "resource_group_name" {
+  value       = azurerm_resource_group.rg.name
+  description = "The name of the resource group"
+}
+
+output "function_app_name" {
+  value       = azurerm_windows_function_app.function_app.name
+  description = "The name of the function app"
+}
+
+output "function_app_default_hostname" {
+  value       = azurerm_windows_function_app.function_app.default_hostname
+  description = "The default hostname of the function app"
+}
+
+output "runtime_storage_account_name" {
+  value       = data.azurerm_storage_account.runtime.name
+  description = "The name of the runtime storage account"
+}
+
+output "data_storage_account_name" {
+  value       = data.azurerm_storage_account.storage.name
+  description = "The name of the data storage account"
+}
+
+output "application_insights_name" {
+  value       = azurerm_application_insights.appinsights.name
+  description = "The name of the Application Insights instance"
+}
+
+output "application_insights_instrumentation_key" {
+  value       = azurerm_application_insights.appinsights.instrumentation_key
+  description = "The instrumentation key of the Application Insights instance"
+  sensitive   = true
+}
+
+output "sql_server_name" {
+  value       = azurerm_mssql_server.sql_server.name
+  description = "The name of the SQL server"
+}
+
+output "sql_server_fqdn" {
+  value       = azurerm_mssql_server.sql_server.fully_qualified_domain_name
+  description = "The fully qualified domain name of the SQL server"
+}
+
+output "sql_database_name" {
+  value       = azurerm_mssql_database.sql_db.name
+  description = "The name of the SQL database"
+}
+
+output "key_vault_name" {
+  value       = azurerm_key_vault.kv.name
+  description = "The name of the Key Vault"
+}
+
+output "key_vault_uri" {
+  value       = azurerm_key_vault.kv.vault_uri
+  description = "The URI of the Key Vault"
+}
+
+output "log_analytics_workspace_name" {
+  value       = azurerm_log_analytics_workspace.loganalytics.name
+  description = "The name of the Log Analytics Workspace"
+}
